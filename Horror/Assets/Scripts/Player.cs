244118b93168dba713b26088328a6d241c656517
Admin1308 <Dmitry130821029199999@mail.ru>
2022-08-03 01:20:59 +0600

    Relise
    Добавил выносливости, еще на доработке

--- Horror/Assets/Scripts/Player.cs
+++ Horror/Assets/Scripts/Player.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Player : MonoBehaviour
 {
     [Header("Mooving Speed Player")]
-    public float speed = 0.1f;
-    public float speedrun = 0.2f;
-   
-  
-    void FixedUpdate()
+    public float speed = 2f;
+    public float speedrun = 4f;
+    public float timeReset = 100;
+    public float startspeed = 0f;
+    private float onfoot = 1.4f;
+
+   void FixedUpdate()
     {
         GetInput();
+        Stamina();
     }
     private void GetInput()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.localPosition += transform.forward* speed * Time.deltaTime;
+            transform.localPosition += transform.forward * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
@@ -31,13 +36,36 @@ public class Player : MonoBehaviour
         {
             transform.localPosition += transform.right * speed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            if (Input.GetKey(KeyCode.W))
-            {
-                transform.localPosition += transform.forward * speedrun * Time.deltaTime;
-            }
-        }
-        
+        if (Input.GetKey(KeyCode.W) &&  && Input.GetKey(KeyCode.LeftShift))
+        {    
+          transform.localPosition += transform.forward * speedrun * Time.deltaTime; 
+        }      
     }
-}
+    private void Stamina()
+    {
+        if (timeReset < 0) timeReset = 0;
+        if (timeReset < 100)
+        {
+            timeReset += 15 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        {
+            timeReset -= 50 * Time.deltaTime;
+        }
+        if (timeReset <= 0)
+        {
+            speedrun = startspeed;
+            speed = onfoot;
+            transform.localPosition += transform.forward * startspeed * Time.deltaTime;
+            Debug.Log("Кончилась выносливость");  
+        }
+        if (timeReset>50)
+        {
+            speedrun = 4f;
+            speed = 2f;
+        }
+    }
+}  
+
+
+
